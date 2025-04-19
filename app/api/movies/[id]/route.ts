47dd2442e8bf9443686b087b74634c212f276d6e
Leia <xuechunlei922@gmail.com>
2025-04-19 15:41:22 +0100

    fix: type issue in api/movies/[id]/route

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const id = params.id
+    const id = (await params).id
 
     if (!id) {
       return NextResponse.json(
